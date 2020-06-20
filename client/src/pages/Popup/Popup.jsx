65b20d46c39b1c2e3c89c50116dddc80b0b4eb56
timkim0713 <timkim0713@gmail.com>
2020-06-21 08:42:05 +0900

    panel

--- client/src/pages/Popup/Popup.jsx
+++ client/src/pages/Popup/Popup.jsx
@@ -2,26 +2,9 @@ import React from 'react';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
-
+import index from '../Content/index.js';
 const Popup = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return index;
 };
 
 export default Popup;
