00bf40aec975cd5065a3e8820b35c9bc435bb058
Tim Kim <timkim0713@gmail.com>
2020-06-21 02:35:29 +0900

    initial client side panel

---
+++ client/src/Components/Modal.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { X, AlignCenter } from "react-feather";
+import Draggable from "react-draggable";
+import { ModalContext } from "../Contexts/ModalProvider";
+
+const Modal = () => {
+  return (
+    <ModalContext.Consumer>
+      {({ windowPosition, extensionId, getExtensionId }) => (
+        <div
+          id="modal"
+          className="modal-window"
+          style={{
+            transform: windowPosition,
+          }}
+        >
+          <div className="modal-window-inner-border">
+            <>
+              <div className="modal-body">
+                <div className="modal-handle">
+                  <div className="modal-close-button">
+                    <X
+                      style={{ margin: 5, paddingTop: 15 }}
+                      color="#5d6484"
+                      size="24"
+                    />
+                  </div>
+
+                  {/* //close button div end */}
+                </div>
+                <p id="mainHeading">CBAACE</p>
+                {/* // modal handle div end */}
+                <div className="modal-content">
+                  <h3>{extensionId}</h3>
+                  <button onClick={getExtensionId} className="modal-button">
+                    Get Extension ID
+                  </button>
+                </div>{" "}
+                {/* modal content end div */}
+              </div>{" "}
+              {/* // modal body end div */}
+            </>
+          </div>{" "}
+          {/* //modal window inner border div */}
+        </div> // first div end
+      )}
+    </ModalContext.Consumer>
+  );
+};
+
+export default Modal;
