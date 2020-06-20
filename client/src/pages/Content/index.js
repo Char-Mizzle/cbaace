1c1af23cf4f17ddc441fff6ba1a50d5709651de1
Sudara Ranasinghe <sudara1999@gmail.com>
2020-06-21 06:24:59 +0900

    side panel works

--- client/src/pages/Content/index.js
+++ client/src/pages/Content/index.js
@@ -1,6 +1,52 @@
-import { printLine } from './modules/print';
+import React, { Component } from 'react';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import ReactDOM from "react-dom";
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+export default class SidePanel extends Component {
+  render() {
+    return (
+        <SideNav
+        onSelect={(selected) => {
+            // Add your code here
+        }}
+    >
+        <SideNav.Toggle />
+        <SideNav.Nav defaultSelected="home">
+            <NavItem eventKey="home">
+                <NavIcon>
+                    <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                </NavIcon>
+                <NavText>
+                    Home
+                </NavText>
+            </NavItem>
+            <NavItem eventKey="charts">
+                <NavIcon>
+                    <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+                </NavIcon>
+                <NavText>
+                    Charts
+                </NavText>
+                <NavItem eventKey="charts/linechart">
+                    <NavText>
+                        Line Chart
+                    </NavText>
+                </NavItem>
+                <NavItem eventKey="charts/barchart">
+                    <NavText>
+                        Bar Chart
+                    </NavText>
+                </NavItem>
+            </NavItem>
+        </SideNav.Nav>
+    </SideNav>
+    );
+  }
+};
 
-printLine("Using the 'printLine' function from the Print Module");
+const app = document.createElement('div');
+document.body.appendChild(app);
+ReactDOM.render(<SidePanel />, app);
+
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -52,7 +52,7 @@ var options = {
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader',
-        exclude: /node_modules/,
+        // exclude: /node_modules/,
       },
       {
         test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
